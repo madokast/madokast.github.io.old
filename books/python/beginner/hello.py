adcdfdb401adab6d0242db1a85889759d025925f
madokast <578562554@qq.com>
2020-10-30 17:19:13 +0800

    back 996

--- books/python/beginner/hello.py
+++ books/python/beginner/hello.py
@@ -1,4 +1,12 @@
-msg = 'hello world'
+def solve(len,num):
+    if num==1:
+        return len
+    else:
+        m = 0
+        for i in range(1,len):
+            m = max(m,solve(len-i,num-1)*i)
+        return m
 
-print(msg)
+ans = solve(8,3)
+print(ans)
 
