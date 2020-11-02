fbf20d9d1bf8566ba96907b2d6b103df07fa1db4
madokast <578562554@qq.com>
2020-11-02 22:36:30 +0800

    py2

--- books/python/beginner/A03集合.py
+++ books/python/beginner/A03集合.py
@@ -99,3 +99,63 @@ a,*x,b = list(range(5))
 printType(a)
 printType(x)
 printType(b)
+
+print(a)
+print(x)
+print(b)
+print(*x)
+
+print(*'zrx')
+
+# SyntaxError: can't use starred expression here
+# t = *[1,2,3]
+# print(t)
+# printType(t)
+
+print(*'123')
+print(*[1,2,3])
+print(*(1,2,3))
+
+m = {}
+print(m)
+
+m = {'name':'mdk'}
+print(m)
+
+m = dict([('name','mdk'),('age',12)])
+print(m)
+
+m['id']=123
+print(m)
+
+m['id']=111
+print(m)
+
+for k in m:
+    print(k,m[k])
+
+print(m.items())
+print(m.values())
+print(m.keys())
+
+for k,v in m.items():
+    print(k,v)
+
+print(m)
+print(m.pop('name'))
+print(m)
+
+arr = [1,1,1,2,3]
+arr = list(set(arr))
+print(arr)
+
+s = {1,2,3}
+s.remove(1)
+print(s)
+
+s.pop()
+print(s)
+
+printType(s)
+s = frozenset(s)
+printType(s)
