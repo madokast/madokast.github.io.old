f6017762b3931e57cc7bb3aa1f9ef3ed2e00d95e
madokast <578562554@qq.com>
2020-11-13 10:57:40 +0800

    geatpy

--- books/Language/cuda/pycuda/A02_device_info.py
+++ books/Language/cuda/pycuda/A02_device_info.py
@@ -6,21 +6,25 @@ Created on Sun Aug 12 11:29:01 2018
 
 import pycuda.driver as drv
 import pycuda.autoinit
+
 drv.init()
 print("%d device(s) found." % drv.Device.count())
 for i in range(drv.Device.count()):
     dev = drv.Device(i)
     print("Device #%d: %s" % (i, dev.name()))
     print("  Compute Capability: %d.%d" % dev.compute_capability())
-    print("  Total Memory: %s GB" % (dev.total_memory()//(1024*1024*1024)))
-    attributes = [(str(prop), value) 
-            for prop, value in list(dev.get_attributes().items())]
+    print("  Total Memory: %s GB" % (dev.total_memory() // (1024 * 1024 * 1024)))
+    attributes = [
+        (str(prop), value) for prop, value in list(dev.get_attributes().items())
+    ]
     attributes.sort()
-    n=0
+    n = 0
     for prop, value in attributes:
-        print("  %s: %s " % (prop, value),end=" ")
-        n = n+1
-        if(n%2 == 0):
+        print("  %s: %s " % (prop, value), end=" ")
+        n = n + 1
+        if n % 2 == 0:
             print(" ")
 
-print(drv.Device(0).get_attributes()[pycuda._driver.device_attribute.MULTIPROCESSOR_COUNT])
+print(
+    drv.Device(0).get_attributes()[pycuda._driver.device_attribute.MULTIPROCESSOR_COUNT]
+)
