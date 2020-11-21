edb5878b2e1b15922c302180666e9b5ca938b641
madokast <578562554@qq.com>
2020-11-21 17:48:51 +0800

    wuenda

--- books/machinelearning/PyTorch/A01HelloPyTorch.py
+++ books/machinelearning/PyTorch/A01HelloPyTorch.py
@@ -1,5 +1,17 @@
 import torch
 
-a = torch.randn(2,2,2)
+x = torch.tensor(1.)
+a = torch.tensor(1.,requires_grad=True)
+b = torch.tensor(2.,requires_grad=True)
+c = torch.tensor(3.,requires_grad=True)
 
-print(a)
+y = a**2 *b + b*x +c
+print(y)
+print(y.grad_fn)
+
+print(a.grad,b.grad,c.grad)
+
+grads = torch.autograd.grad(y,[a,b,c])
+
+
+print(grads)
