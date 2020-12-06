dad2e74eb6003275314dc37305dd326fc38173f4
madokast <578562554@qq.com>
2020-12-06 14:03:47 +0800

    compute

--- books/Language/cuda/pycuda/A01_hello_world.py
+++ books/Language/cuda/pycuda/A01_hello_world.py
@@ -4,13 +4,22 @@ import pycuda.autoinit
 import pycuda.driver as drv
 from pycuda.compiler import SourceModule
 
+# 内核函数定义
 mod = SourceModule("""
-    #include <stdio.h>
-    __global__ void hello_kernel(){
-        printf("hello, pycuda! -- from kernel in device");
+    // 需要从 CPU 端调用，让 GPU 执行的函数称为内核函数，需要用 __global__ 修饰
+    __global__ void kernel_function(){
+        printf("hello, world! -- from gpu\\n");
     }
 """)
 
-function = mod.get_function("hello_kernel")
-function(block=(1, 1, 1))
-print("hello, pycuda! -- from python in host")
+# 获取内核函数
+kernel_function = mod.get_function("kernel_function")
+
+# 调用内核函数
+kernel_function(grid=(1,1,1), block=(1,1,1))
+print("hello, pycuda! -- from python")
+
+"""
+注意：jupyter notebook 中没有打印 GPU 运行信息，可能是监听的 IO 流不对
+在独立的 py 文件中，能收到 GPU 的打印
+"""
