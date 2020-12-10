e5906e255ab74dc0b3f7e51e43bb1162ef7bfd9a
madokast <578562554@qq.com>
2020-12-10 23:05:40 +0800

    kirei

--- books/machinelearning/dive_into_deep_learning/d2lzh.py
+++ books/machinelearning/dive_into_deep_learning/d2lzh.py
@@ -2,6 +2,8 @@ import random
 from mxnet import autograd, nd
 from IPython import display
 from matplotlib import pyplot as plt
+import sys
+from mxnet.gluon import data as gdata
 
 def hello():
     print("hello")
@@ -49,6 +51,29 @@ def sgd(params, lr, batch_size):
     for param in params:
         param[:] = param - lr * param.grad / batch_size
 
+
+# 读取数据集 Fashion-MNIST 
+def load_data_fashion_mnist(batch_size):
+    # 训练数据
+    mnist_train = gdata.vision.FashionMNIST(train=True)
+    # 测试数据
+    mnist_test = gdata.vision.FashionMNIST(train=False)
+
+
+    transformer = gdata.vision.transforms.ToTensor()
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+
+    train_iter = gdata.DataLoader(mnist_train.transform_first(transformer),
+                                batch_size, shuffle=True,
+                                num_workers=num_workers)
+    test_iter = gdata.DataLoader(mnist_test.transform_first(transformer),
+                                batch_size, shuffle=False,
+                                num_workers=num_workers)
+    return train_iter, test_iter
+
 # Fashion-MNIST 数据标签含义对照
 def get_fashion_mnist_labels(labels):
     text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
@@ -65,3 +90,39 @@ def show_fashion_mnist(images, labels):
         f.set_title(lbl)
         f.axes.get_xaxis().set_visible(False)
         f.axes.get_yaxis().set_visible(False)
+
+def evaluate_accuracy(data_iter, net):
+    acc_sum, n = 0.0, 0
+    for X, y in data_iter:
+        y = y.astype('float32')
+        acc_sum += (net(X).argmax(axis=1) == y).sum().asscalar()
+        n += y.size
+    return acc_sum / n
+
+# 图象分类数据集 Fashion-MNIST
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+              params=None, lr=None, trainer=None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            with autograd.record():
+                y_hat = net(X)
+                l = loss(y_hat, y).sum()
+            l.backward()
+            if trainer is None:
+                sgd(params, lr, batch_size)
+            else:
+                trainer.step(batch_size)  # “softmax回归的简洁实现”一节将用到
+            y = y.astype('float32')
+            train_l_sum += l.asscalar()
+            train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()
+            n += y.size
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+def xyplot(x_vals, y_vals, name):
+    set_figsize(figsize=(5, 2.5))
+    plt.plot(x_vals.asnumpy(), y_vals.asnumpy())
+    plt.xlabel('x')
+    plt.ylabel(name + '(x)')
